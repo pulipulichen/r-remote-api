e0769851deee56a0aee5ec91bcb7c859ddf61f1d
Pulipuli Chen <pulipuli.chen@gmail.com>
2016-11-09 01:09:54 +0800

    1108 在寫blog了

--- client/demo.php
+++ client/demo.php
@@ -1,19 +1,22 @@
 <?php
-
+// 載入R Remote API Client端函式
 include 'r-remote-api-client.php';
 
-//$remote_api_url = "http://localhost/r-remote-api/server/?";
-$remote_api_url = "http://192.168.56.152/r-remote-api.php";
+// R Remote API Server端接口
+//$remote_api_url = "http://192.168.56.152/r-remote-api.php";
+$remote_api_url = "http://192.168.11.115/r-remote-api.php";
 
 // ---------------------------
 // 計算資料的展示
-$rscript = file_get_contents("demo-message.R");
-$parameters = array(20, 80);
+$rscript = file_get_contents("demo-message.R"); // 取得R Script
+$parameters = array(20, 80); // 計算兩個數值的平均
 
+// 取得平均數計算結果
 echo "Result: " . rRemoteAPI($remote_api_url, $rscript, $parameters);
 
 // ---------------------------
 // 顯示圖表的展示
-$rscript_plot = file_get_contents("demo-plot.R");
+$rscript_plot = file_get_contents("demo-plot.R"); // 取得R Script
 
+// 取得圖表結果
 echo '<img src="' . rRemoteAPI($remote_api_url, $rscript_plot) . '" />';
