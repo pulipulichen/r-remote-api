bb4943ab653e8e89beed420ad2112578b4469ec5
Pulipuli Chen <pulipuli.chen@gmail.com>
2016-11-09 01:08:12 +0800

    1108 在寫blog了

--- client/demo-message.R
+++ client/demo-message.R
@@ -1,7 +1,16 @@
+# 取得圖表路徑跟輸入的parameters
 args <- commandArgs(TRUE)
 
+# 因為args[1]是圖表的檔案路徑及名稱，在此不能使用
+# 實際上paramters是從args[2]開始計算
+
+# 將parameters第一個參數設為最小值，用strtoi()將字串轉換成整數
 min<-strtoi(args[2])
+# 將parameters第二個參數設為最大值，用strtoi()將字串轉換成整數
 max<-strtoi(args[3])
-cat(mean(c(min:max)))
-#mean(c(0:10,50)
-#cat(args[2])
+
+# 用c()組成陣列
+data<-c(min:max)
+
+# 用mean()計算平均，並以cat()輸出。用cat()輸出則不會顯示row name
+cat(mean(data))
--- client/demo-plot.R
+++ client/demo-plot.R
@@ -1,7 +1,11 @@
+# 取得圖表路徑跟輸入的parameters
 args <- commandArgs(TRUE)
+
+# 將args[1]設為圖表的檔案路徑及名稱
 plot.filename <- args[1]
+# 設置將圖表儲存到以下路徑
+png(plot.filename) 
 
-x <- rnorm(1006,0,1)
-
-png(plot.filename)
+# 用亂數繪製直方圖
+x <- rnorm(100,0,1)
 hist(x, col="lightblue")
